8f5d266627ec4ef2b8bc2d590ef630e17e25a775
sathvik89 <koriginjasathvik@gmail.com>
2025-04-09 10:46:56 +0530

    addedskill

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -7,6 +7,8 @@ import JavaScriptImage from "../Images/JAVASCRIPT.png";
 import PythonImage from "../Images/Python.png";
 import ReactImage from "../Images/REACT.png";
 import TailwindImage from "../Images/TAILWIND.png";
+import FigmaImage from "../Images/FIGMA.png";
+import PandaImage from "../Images/PANDAS.png";
 import styles from "../Styling/Skills.module.css";
 export default function Skills() {
   return (
@@ -20,32 +22,36 @@ export default function Skills() {
         <div className={styles.skillname}>CSS</div>
       </div>
       <div className={styles.skillsinner}>
-        <img className={styles.skillimage} src={DSAImage} alt="" />
-        <div className={styles.skillname}>Data structures and algorithms</div>
-      </div>
-      <div className={styles.skillsinner}>
         <img className={styles.skillimage} src={GitImage} alt="" />
         <div className={styles.skillname}>Git</div>
       </div>
-
+      <div className={styles.skillsinner}>
+        <img className={styles.skillimage} src={TailwindImage} alt="" />
+        <div className={styles.skillname}>Tailwind CSS</div>
+      </div>
       <div className={styles.skillsinner}>
         <img className={styles.skillimage} src={JavaScriptImage} alt="" />
         <div className={styles.skillname}>JavaScript</div>
       </div>
-
       <div className={styles.skillsinner}>
         <img className={styles.skillimage} src={PythonImage} alt="" />
         <div className={styles.skillname}>Python</div>
       </div>
-
+      <div className={styles.skillsinner}>
+        <img className={styles.skillimage} src={DSAImage} alt="" />
+        <div className={styles.skillname}>Data structures and algorithms</div>
+      </div>
       <div className={styles.skillsinner}>
         <img className={styles.skillimage} src={ReactImage} alt="" />
         <div className={styles.skillname}>React</div>
       </div>
-
       <div className={styles.skillsinner}>
-        <img className={styles.skillimage} src={TailwindImage} alt="" />
-        <div className={styles.skillname}>Tailwind CSS</div>
+        <img className={styles.skillimage} src={FigmaImage} alt="" />
+        <div className={styles.skillname}>Figma</div>
+      </div>
+      <div className={styles.skillsinner}>
+        <img className={styles.skillimage} src={PandaImage} alt="" />
+        <div className={styles.skillname}>Pandas</div>
       </div>
     </div>
   );
